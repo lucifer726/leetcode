8d27a007f03300aac725d119f31f9483f1ab69ca
lucifer726 <18856419225@163.com>
2020-09-27 21:14:02 +0800

    1. 两数之和

--- 200927.py
+++ 200927.py
@@ -1,6 +1,6 @@
-# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(nums, target):
         for i in range(len(nums)):
             a = target - nums[i]
             if a in nums:
@@ -11,3 +11,5 @@ class Solution:
                     break
             else:
                 continue
+
+print(Solution.twoSum([2,7,11,15],9))
